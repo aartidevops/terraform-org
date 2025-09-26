dae86e1b045642c27b79e1f9fe9ea0a1b83a7ea6
Aarti chaple <aartichaple2124@gmail.com>
2025-09-26 16:10:12 +0530

    test

---
+++ workstation/main.tf
@@ -0,0 +1,112 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "RG"
+  location = "Southeast Asia"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "Default"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+# Public IP
+resource "azurerm_public_ip" "publicip" {
+  name                = "linux-pip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "linux-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.publicip.id
+  }
+}
+
+# Network Security Group
+resource "azurerm_network_security_group" "nsg" {
+  name                = "linux-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  # Allow all inbound
+  security_rule {
+    name                       = "Allow-All-Inbound"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  # Allow all outbound
+  security_rule {
+    name                       = "Allow-All-Outbound"
+    priority                   = 200
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Associate NSG to NIC
+resource "azurerm_network_interface_security_group_association" "nsgassoc" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# Linux VM
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                            = "workstation"
+  resource_group_name             = azurerm_resource_group.rg.name
+  location                        = azurerm_resource_group.rg.location
+  size                            = "Standard_DS1_v2"
+  admin_username                  = "Aarti"
+  admin_password                  = "Aarti@431721"
+  disable_password_authentication = false
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  # ✅ Red Hat image
+  source_image_reference {
+    publisher = "RedHat"
+    offer     = "RHEL"
+    sku       = "87-gen2"   # Use "93-gen2" if you want RHEL 9.3
+    version   = "latest"
+  }
+
+  tags = {
+    environment = "staging"
+  }
+}
+
+
