2b1dcd03c431b0681a0cead761818489f832aa88
Aarti chaple <aartichaple2124@gmail.com>
2025-09-27 15:25:45 +0530

    test

--- workstation/provider.tf
+++ workstation/provider.tf
@@ -1,3 +1,12 @@
 provider "azurerm" {
   features {}
 }
+
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "RG"
+    storage_account_name  = "rttfstate"
+    container_name        = "tfstatecontainer"
+    key                   = "terraform.tfstate"
+  }
+}
